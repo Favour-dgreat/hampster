a7abbfb3333f9a9ba166178258939148805fbf94
Adeshina Favour Adekunle <53022031+Favour-dgreat@users.noreply.github.com>
2024-10-16 18:18:08 +0100

    fixes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,11 @@
+document.addEventListener('mousemove', function(e) {
+  const wandContainer = document.querySelector('.magic-wand-container');
+  wandContainer.style.left = e.pageX + 'px';
+  wandContainer.style.top = e.pageY + 'px';
+});
 window.scrollTo(0, 0);
 function copyPress(copy) {
     copy ? navigator.clipboard.writeText(copy) : console.log('error copy');
 }
+
+  
